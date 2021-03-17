8cc49163edea312106764b50da6028c1ec3704ec
abirhasn07 <iamabirhasan07@gmail.com>
2021-03-18 02:14:42 +0600

    March Month Project Challange

---
+++ script.js
@@ -0,0 +1,26 @@
+var sideNav = document.getElementById('side-nav')
+var menuBtn = document.getElementById("menu-btn")
+var menu = document.getElementById("menu")
+var closeMenu = document.getElementById("close-menu")
+
+menuBtn.onclick = function() {
+
+    if (sideNav.style.right == "-250px") {
+        sideNav.style.right = "0";
+
+
+    } else {
+        sideNav.style.right = "-250px";
+
+        sideNav.style.display = "block"
+
+    }
+}
+
+// menuBtn.addEventListener("click", function() {
+//     if (sideNav.style.display = "block") {
+//         sideNav.style.display = "none"
+//     } else {
+//         sideNav.style.display = "block"
+//     }
+// })
